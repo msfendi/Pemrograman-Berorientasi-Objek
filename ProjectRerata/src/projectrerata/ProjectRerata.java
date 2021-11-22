66ccb817d7d1a570c9173d9731a527cdab6d25c4
MohamadSyarifudinEfendi-105 <efendisyarifudin1@gmail.com>
2021-11-22 23:44:31 +0700

    Praktikum 11

---
+++ ProjectRerata/src/projectrerata/ProjectRerata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectrerata;
+
+/**
+ *
+ * @author User
+ */
+public class ProjectRerata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Rerata r = new Rerata();
+//        r.input();
+//        r.output();
+
+//        ArrayProcessing ap = new ArrayProcessing();
+//        ap.input();
+//        ap.output();
+
+        BubleSort bs = new BubleSort();
+        bs.input();
+        bs.output();
+
+    }
+    
+}
